3ace4891ff1abdec144dd571d9afe312fb6fbd9e
Mati <mbarrionuevo@simtlix.com>
2021-03-29 18:32:22 -0300

    deploy

---
+++ doc/precache-manifest.fc6a216e6bc8d9e2a3a600ba628a14de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f357bca9f5c064f6fa30307f890eeb61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15b88bfbf74c212154a5",
+    "url": "/static/css/main.0b081d03.chunk.css"
+  },
+  {
+    "revision": "753086dd47e7eba48477",
+    "url": "/static/js/2.14a6d87f.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.14a6d87f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b88bfbf74c212154a5",
+    "url": "/static/js/main.56bf3248.chunk.js"
+  },
+  {
+    "revision": "6e76b1754a6e6c329ffb",
+    "url": "/static/js/runtime-main.50fb5f2f.js"
+  }
+]);
